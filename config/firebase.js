01fa30780a8669d4706c7ebb8c548a4e3d0dd8cf
theAkCode <abhishekkrbtw@gmail.com>
2025-08-17 11:58:26 +0530

    send

--- config/firebase.js
+++ config/firebase.js
@@ -5,13 +5,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_OkfQtT5kFhF1ywo5le5SgLNn-QVBTlM",
-  authDomain: "commcoll1-e9a4b.firebaseapp.com",
-  projectId: "commcoll1-e9a4b",
-  storageBucket: "commcoll1-e9a4b.firebasestorage.app",
-  messagingSenderId: "501958977070",
-  appId: "1:501958977070:web:b8950ce3b78b7aa36961bf",
-  measurementId: "G-55BE0CP0Q9"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+
 };
 
 
